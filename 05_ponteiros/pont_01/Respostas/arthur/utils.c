30676880590fe27312e923ecf4bd7f03a6d90a3c
Arthur <arthurmanelli@hotmail.com>
2025-11-25 11:00:26 -0300

    pont_01

---
+++ 05_ponteiros/pont_01/Respostas/arthur/utils.c
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+int TrocaInterios(int * n1, int * n2) {
+    if (*n1 <= *n2) return 0;    
+    
+    int aux;
+    aux = *n1;
+    *n1 = *n2;
+    *n2 = aux;
+
+    return 1;
+
+}
